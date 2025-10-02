562a976217bbef7c5f6e784a9d75401d06d01786
DatTien <dat.nguyen.bk2006@gmail.com>
2025-10-02 17:20:21 +0700

    Checked SimHash

--- header/VectorRecord.h
+++ header/VectorRecord.h
@@ -5,11 +5,13 @@ struct VectorRecord
 {
     int id;
     vector<double> vec;
-    VectorRecord(int id_, vector<double> vec_):id(id_), vec(vec_) {}
+    VectorRecord(int id_, vector<double> vec_) : id(id_), vec(vec_) {}
     bool operator==(const VectorRecord &other)
     {
         return this->vec == other.vec && this->id == other.id;
     }
+
+    VectorRecord() : id(0), vec(vector<double>()) {}
 
     bool operator!=(const VectorRecord &other)
     {
@@ -22,5 +24,19 @@ struct VectorRecord
         this->vec = other.vec;
         this->id = other.id;
         return *this;
+    }
+
+    string to_string() const
+    {
+        stringstream ss;
+        ss << "ID: " << id << ", Vector: [";
+        for (size_t i = 0; i < vec.size(); ++i)
+        {
+            ss << vec[i];
+            if (i < vec.size() - 1)
+                ss << ", ";
+        }
+        ss << "]";
+        return ss.str();
     }
 };
---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include "header/SimHash.h"
+#include "header/MurmurHash3.h"
+#include <cmath>
+
+int main()
+{
+    // Example usage of SimHash
+    SimHash simHash;
+
+    vector<double> vec1 = {1.0, 2.0, 3.0};
+    vector<double> vec2 = {1.0, 2.0, 3.4};
+
+    // Create a vector recordcls
+
+    VectorRecord record1(1, vec1);
+    VectorRecord record2(2, vec2);
+
+    // Hash a single vector record
+    VectorRecord hashedRecord = simHash.hash_1(record1);
+    VectorRecord hashedRecord2 = simHash.hash_1(record2);
+
+    // Print the hashed vector record
+    cout << "Hashed Record 1: " << hashedRecord.to_string() << endl;
+    cout << "Hashed Record 2: " << hashedRecord2.to_string() << endl;
+
+    cout << (hashedRecord2 == hashedRecord) << endl;
+    return 0;
+}
+
+// g++ -std=c++17 -O2 -Iheader -o my_program main.cpp source/MurmurHash3.cpp source/SimHash.cpp
